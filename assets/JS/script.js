b5006ce99f325bc826969ce7d57f9d832ef7bf5f
guillaumedewolf <guillaumedewolf@gmail.com>
2018-01-22 14:10:30 +0100

    toggle serie et film

---
+++ assets/JS/script.js
@@ -0,0 +1,25 @@
+
+//afficher plus de film boutton
+
+$( "#filmBoutton" ).click(function() {
+  $( "#moviesCacher" ).toggle();
+  if($("#moviesCacher").css("display") == "none"){
+	$("#filmBoutton").html("PLUS DE FILM")
+	}
+	else{
+	$("#filmBoutton").html("MOINS DE FILM")
+	}
+});
+
+//affucher plus de serie boutton
+
+$( "#serieBoutton" ).click(function() {
+  $( "#seriesCacher" ).toggle();
+  if($("#seriesCacher").css("display") == "none"){
+	$("#serieBoutton").html("PLUS DE SERIES")
+	}
+	else{
+	$("#serieBoutton").html("MOINS DE SERIES")
+	}
+});
+
