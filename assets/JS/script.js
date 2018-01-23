75bbfe03b672f419fe95d3a096708be4db9b7611
Corentin W <[corentinwarin@gmail.com]>
2018-01-23 14:54:47 +0100

    css+menuscroll

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,3 +1,33 @@
+// page menu scroll 
+$(document).ready(function(){
+  // Initialize Tooltip
+  $('[data-toggle="tooltip"]').tooltip();
+
+  // Add smooth scrolling to all links in navbar + footer link
+  $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 900, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+})
+
 // les variables
 var moviesEcritureGenre = ""
 var moviesEcritureGenreCacher = ""
@@ -95,11 +125,11 @@ var films = [
  	$("#moviesJS").html(moviesEcritureGenre)
 
  }
- 
 
 
 
- 
+
+
 
 //afficher tous les films (+ de 12 film)
 $("#all").click(function(){
@@ -126,12 +156,12 @@ function allMovies(){
  	$("#moviesJS").html(moviesEcritureGenre)
 
  }
- 	
+
 
 
 
 allMovies()
- 
+
 
 
 
@@ -149,17 +179,3 @@ function popup(valeur){
 	$("#popupFilm").html(ecrirePopup)
 	$("#popupFilm").css("display","block")
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
