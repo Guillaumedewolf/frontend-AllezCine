38b8ecef5c590f46827bf10c33dc87821fa72814
Corentin W <[corentinwarin@gmail.com]>
2018-02-06 09:31:05 +0100

    final fan

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -460,18 +460,18 @@ lastedFilm()
 // formulaire modal
 
 $("#bouttonFormulaire").click(function(){
-	var firstName = $("#firstName").val() 
-	var lastName = $("#lastName").val() 
-	var email = $("#email").val() 
-	var subject = $("#subject").val() 
-	var comments = $("#comments").val() 
+	var firstName = $("#firstName").val()
+	var lastName = $("#lastName").val()
+	var email = $("#email").val()
+	var subject = $("#subject").val()
+	var comments = $("#comments").val()
 
 
 	$("#nameFormulaire").html(firstName +" "+ lastName)
 	$("#emailFormulaire").html(email)
 	$("#sujetFormulaire").html(subject)
 	$("#contenuFormulaire").html(comments)
-	
+
 
 	$("#modalformulaire").modal("show")
 
